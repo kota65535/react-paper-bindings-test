2e57b5fdbac1cba91934c6b9d92073a024f7a065
kota65535 <kota65535@gmail.com>
2018-02-10 03:18:04 +0900

    webpack config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -158,6 +158,19 @@ module.exports = {
               },
             ],
           },
+          // Process JS with Babel.
+          {
+            test: /\.(js|jsx|mjs)$/,
+            include: paths.appSrc,
+            loader: require.resolve('babel-loader'),
+            options: {
+
+              // This is a feature of `babel-loader` for webpack (not Babel itself).
+              // It enables caching results in ./node_modules/.cache/babel-loader/
+              // directory for faster rebuilds.
+              cacheDirectory: true,
+            },
+          },
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
